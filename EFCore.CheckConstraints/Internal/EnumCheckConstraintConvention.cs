b6162ed8bc499b1bc9489eef14194e9e8aebb05e
Joe Cellucci <celluj34@gmail.com>
2023-01-16 22:30:08 -0600

    simplify check

--- EFCore.CheckConstraints/Internal/EnumCheckConstraintConvention.cs
+++ EFCore.CheckConstraints/Internal/EnumCheckConstraintConvention.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -16,8 +14,6 @@ public class EnumCheckConstraintConvention : IModelFinalizingConvention
 {
     private readonly IRelationalTypeMappingSource _typeMappingSource;
     private readonly ISqlGenerationHelper _sqlGenerationHelper;
-    private readonly Regex _castRegex = new("CAST\\((\\d+) AS (?:tinyint|smallint|bigint)\\)", RegexOptions.Compiled | RegexOptions.Singleline);
-    private readonly Regex _decimalRegex = new("(\\d+).0", RegexOptions.Compiled | RegexOptions.Singleline);
 
     public EnumCheckConstraintConvention(
         IRelationalTypeMappingSource typeMappingSource,
@@ -54,8 +50,8 @@ public class EnumCheckConstraintConvention : IModelFinalizingConvention
                     continue;
                 }
 
-                var enumValues = Enum.GetValues(propertyType).Cast<object>().Select(typeMapping.GenerateSqlLiteral).ToList();
-                if (enumValues.Count == 0)
+                var values = Enum.GetValues(propertyType);
+                if (values.Length == 0)
                 {
                     continue;
                 }
@@ -64,7 +60,7 @@ public class EnumCheckConstraintConvention : IModelFinalizingConvention
 
                 sql.Append(_sqlGenerationHelper.DelimitIdentifier(columnName));
 
-                if (TryParseMinAndMax(enumValues, out var minValue, out var maxValue))
+                if (TryParseContiguousRange(values, typeMapping, out var minValue, out var maxValue))
                 {
                     sql.Append(" BETWEEN ");
                     sql.Append(minValue);
@@ -74,7 +70,7 @@ public class EnumCheckConstraintConvention : IModelFinalizingConvention
                 else
                 {
                     sql.Append(" IN (");
-                    foreach (var item in enumValues)
+                    foreach (var item in values.Cast<object>().Select(typeMapping.GenerateSqlLiteral))
                     {
                         sql.Append(item);
                         sql.Append(", ");
@@ -90,41 +86,24 @@ public class EnumCheckConstraintConvention : IModelFinalizingConvention
         }
     }
 
-    // we use decimal explicitly here because decimal is a wider number type than all of the valid enum backing types
-    private bool TryParseMinAndMax(IReadOnlyCollection<string> enumValues, out decimal? minValue, out decimal? maxValue)
+    private static bool TryParseContiguousRange(
+        IEnumerable values,
+        CoreTypeMapping typeMapping,
+        out decimal minValue,
+        out decimal maxValue)
     {
-        var parsedEnumValues = enumValues.Select<string, decimal?>(
-                x =>
-                {
-                    decimal? result = null;
-
-                    if (decimal.TryParse(x, out var parsed))
-                    {
-                        result = parsed;
-                    }
-                    else if (_decimalRegex.Match(x) is { Success: true } decimalMatch)
-                    {
-                        result = decimal.Parse(decimalMatch.Groups[1].Value);
-                    }
-                    else if (_castRegex.Match(x) is { Success: true } castMatch)
-                    {
-                        result = decimal.Parse(castMatch.Groups[1].Value);
-                    }
-
-                    //decimal.Truncate is necessary because `ulong`s will include a fractional part
-                    return result.HasValue ? decimal.Truncate(result.Value) : null;
-                })
-            .ToList();
-
-        if (parsedEnumValues.Any(x => x is null))
+        if (typeMapping.Converter?.ProviderClrType == typeof(string))
         {
-            minValue = null;
-            maxValue = null;
+            minValue = 0;
+            maxValue = 0;
+
             return false;
         }
 
-        minValue = parsedEnumValues.Min();
-        maxValue = parsedEnumValues.Max();
+        var enumValues = values.Cast<object>().Select(Convert.ToDecimal).ToList();
+
+        minValue = enumValues.Min();
+        maxValue = enumValues.Max();
 
         return maxValue - minValue == enumValues.Count - 1;
     }
